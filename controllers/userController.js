f4a13694efcc5dc581abae01cecf076b1b4ee0cb
yedogwalwal <zxcv12045@naver.com>
2021-03-24 19:22:48 +0900

    ~28

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,14 @@
-export const join = (req, res) => res.render("Join");
-export const login = (req, res) => res.render("Login");
-export const logout = (req, res) => res.render("Logout");
-export const users = (req, res) => res.render("Users");
-export const userDetail = (req, res) => res.render("User Detail");
-export const editProfile = (req, res) => res.render("Edit Profile");
-export const changePassword = (req, res) => res.render("Change Password");
+export const join = (req, res) => 
+  res.render("join", {pageTitle: "Join"});
+export const login = (req, res) => 
+  res.render("login", {pageTitle: "Login"});
+export const logout = (req, res) => 
+  res.render("logout", {pageTitle: "Logout"});
+export const users = (req, res) => 
+  res.render("users", {pageTitle: "Users"});
+export const userDetail = (req, res) => 
+  res.render("userDetail", {pageTitle: "User Detail"});
+export const editProfile = (req, res) => 
+  res.render("editProfile", {pageTitle: "Edit Profile"});
+export const changePassword = (req, res) => 
+  res.render("changePassword", {pageTitle: "Change Password"});
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -9,9 +9,9 @@ import {
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, users);
-userRouter.get(routes.userDetail, userDetail);
+//userRouter.get(routes.users, users);
 userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
